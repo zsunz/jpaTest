e1d5667305c9e7e57fe701df199acb183ec7b323
zsunz <komlov@naver.com>
2024-07-28 17:06:26 +0900

    dd

---
+++ src/main/java/com/jpatest/jpatest/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jpatest.jpatest.service.posts;
+
+import com.jpatest.jpatest.domain.posts.PostsRepository;
+import com.jpatest.jpatest.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
